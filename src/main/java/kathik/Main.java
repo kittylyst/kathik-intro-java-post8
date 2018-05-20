d83e8ba57b0d67378f8cafa5b94102f6a176a1e1
Ben Evans <benjamin.john.evans@gmail.com>
2018-05-20 18:28:30 +0100

    Update tasks

--- src/main/java/kathik/Main.java
+++ src/main/java/kathik/Main.java
@@ -12,29 +12,9 @@ public final class Main {
     }
 
     public void run() {
-        // To get a sequence of temperatures we must parse the CSV according to the following rules:
-        // 1.	ignore lines starting with # that correspond to comments;
-        // 2.	filter alternate lines
-        // 3.	extract the third value corresponding to temperature in Celsius;
-        // 4.	convert to int
+        // 1. Setup a modular build for the application, using a module-info.java to build a single app module
 
-        // Use the stream API to calculate:
-
-        //   (i) Average temp over a month
-
-        //   (ii) Average windspeed over a month.
-
-        // A. Build test cases using the provided CSV file
-
-        // B. Use the live URL provided, and the Asynch HTTP client mentioned in the POM file, to get live data
-        // E.g. the asyncHttpClient() method
-
-        // C. Customize to a location (& time period of interest)
-
-        // D. Extend to a 1-degree x 1-degree area, rather than a single point.
-
-        //   (i) How fine a grid will the data support?
-
-        //   (ii) Design an efficient parallel approach - does this improve the calculation?
+        // 2. Take the existing code and port to use the new HTTP/2 API provided in (version 10) in an incubator
+        // module and remove the dependency on AsynchHTTPClient
     }
 }
